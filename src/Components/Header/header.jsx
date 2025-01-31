930745029550a9eeac353a1e7c590fda8a7d0963
njugus <kayvohnjuguna@gmail.com>
2025-01-31 10:16:27 -0800

    sets up a humburger menu for responsiveness on small screen sizes

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,24 +1,61 @@
-import './header.css'
+// import './header.css'
+
+// const Header = () => {
+//     return (
+//         <>
+//         <header className = 'header'>
+//             <section className='portfolio-header-name'>
+//                 <h1>K<span>elvin</span></h1>
+//             </section>
+//             <nav className = 'navbar'>
+//                 <ul className='portfolio-header-links'>
+//                     <li>Home</li>
+//                     <li>About</li>
+//                     <li>Services</li>
+//                     <li>Tech Stack</li>
+//                     <li>Contact</li>
+//                 </ul>
+//             </nav>
+//         </header>
+//         </>
+//     )
+// }
+
+// export default Header
+
+import { useState } from "react";
+import "./header.css";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function toggleMenu(){
+        setMenuOpen(prevState => !prevState)
+        console.log(menuOpen);
+    }
+
     return (
         <>
-        <header className = 'header'>
-            <section className='portfolio-header-name'>
-                <h1>K<span>elvin</span></h1>
-            </section>
-            <nav className = 'navbar'>
-                <ul className='portfolio-header-links'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Services</li>
-                    <li>Tech Stack</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
+            <header className="header">
+                <section className="portfolio-header-name">
+                    <h1>K<span>elvin</span></h1>
+                </section>
+                <nav className="navbar">
+                    <ul className={`portfolio-header-links ${menuOpen ? "active" : ""}`}>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Services</li>
+                        <li>Tech Stack</li>
+                        <li>Contact</li>
+                    </ul>
+                    {/* Hamburger Menu Icon */}
+                    <div className="hamburger" onClick={toggleMenu}>
+                        ☰
+                    </div>
+                </nav>
+            </header>
         </>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
